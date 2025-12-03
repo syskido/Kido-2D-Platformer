454cb266fab3e1ca6c12abb9cb39fdb5cc708b2c
kido <syskido82@gmail.com>
2025-12-03 20:53:23 +0900

    Space[점프 추가] or Gravity[중력 1 -> 5]

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,5 +13,8 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        if (Input.GetKey(KeyCode.Space)) 
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);        
     }
 }
