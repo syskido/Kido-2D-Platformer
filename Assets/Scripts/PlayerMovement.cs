cc43a6c2f8ff97bcc89826b5021a811d7654d2e0
kido <syskido82@gmail.com>
2025-12-04 14:10:48 +0900

    wallLayer 추가 기둥 세워서 길을 막음

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer; // LayerMask ���
+    [SerializeField] private LayerMask wallLayer; // LayerMask ���
     private Rigidbody2D body;
     private Animator anim; // Animation �� Animator�� ����
-    private bool grounded;
+    private BoxCollider2D boxCollider; // BoxCollider2D ���
 
     private void Awake()
     {
         //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>(); // Animation �� Animator�� ����
+        boxCollider = GetComponent<BoxCollider2D>(); // BoxCollider2D ���
     }
 
     private void Update()
@@ -25,23 +28,36 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
             Jump();
 
         anim.SetBool("run", horizontalInput != 0); // Animator���� SetBool ���
-        anim.SetBool("grounded", grounded); // Animator���� SetBool ���
+        anim.SetBool("grounded", isGrounded()); // Animator���� SetBool ���
+
+        print(onWall());
     }
 
     private void Jump()
     {
         body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
         anim.SetTrigger("jump"); // Animator���� SetTrigger ���
-        grounded = false;
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
-            grounded = true;
+        
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
