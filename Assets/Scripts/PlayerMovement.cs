80f03558f28db97e4be71a83054197edcd1e930e
kido <syskido82@gmail.com>
2025-12-04 12:23:45 +0900

    계속 문제가 생겨
    하지만 또 문제를 해결 했습니다.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,29 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim; // Animation �� Animator�� ����
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); // Animation �� Animator�� ����
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y); // linearVelocity �� velocity�� ����
 
         // Flip player when moving left/right
-        if (horizontalInput> 0.01f)
+        if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
-        else if(horizontalInput < -0.01f)
+        else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space)) 
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);        
+        if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed); // linearVelocity �� velocity�� ����
+
+        anim.SetBool("run", horizontalInput != 0); // Animator���� SetBool ���
     }
 }
